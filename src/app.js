debab3c58b3fa11b11d6a2cb72087c20108609ba
sanje <sanjeevjha882@gmail.com>
2022-07-06 20:37:14 +0530

    module1-modification1

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get("/",async(req,res)=>{
+app.get("/",(req,res)=>{
     res.render("index.ejs",{title:"Index"});
 })
 

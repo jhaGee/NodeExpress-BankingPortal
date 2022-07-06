575bfcb74ed75c9e52486089891ee5f77fb31074
sanje <sanjeevjha882@gmail.com>
2022-07-06 20:14:48 +0530

    module1-changes

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
+
+app.get("/",async(req,res)=>{
+    res.render("index.ejs");
+})
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Listening on port 3000")
+})
+

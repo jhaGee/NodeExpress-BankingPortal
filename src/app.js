4308ff8ecde818ae2b15ca505fc49316731eee44
sanje <sanjeevjha882@gmail.com>
2022-07-06 23:48:46 +0530

    module2-modification3

--- src/app.js
+++ src/app.js
@@ -9,37 +9,18 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const accountData = fs.readFileSync("./src/json/accounts.json", {
-  encoding: "utf8",
-  flag: "r",
-});
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
-const userData = fs.readFileSync("./src/json/users.json", {
-  encoding: "utf8",
-  flag: "r",
-});
+
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
 const users = JSON.parse(userData);
 
-app.get("/", (req, res) =>
-  res.render("index", { title: "Account Summary", accounts: accounts })
-);
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
-app.get("/savings", (req, res) => {
-  app.render("account.ejs", { accounts: accounts.saving });
-});
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 
-app.get("/checking", (req, res) => {
-  app.render("account.ejs", { accounts: accounts.checking });
-});
+app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
-app.get("/credit", (req, res) => {
-  app.render("account.ejs", { accounts: accounts.credit });
-});
-
-app.get("/profile", (req, res) => {
-  app.render("profile.ejs", { user: users[0] });
-});
-
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+app.listen(3000, () => { console.log('PS Project Running on port 3000!') }); 
